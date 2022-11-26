00a33e3e0775c6513399f964701fa238e4d43f22
MochiJump <35511163+MochiJump@users.noreply.github.com>
2022-11-25 22:47:43 -0700

    Update DnsIpParityEnforcer.go

--- DnsIpParityEnforcer.go
+++ DnsIpParityEnforcer.go
@@ -7,12 +7,13 @@ import (
     "io/ioutil"
     "net/http"
     "time"
+    "net"
 )
 
 var prevIp = "0.0.0.0"
 
 func main() {
-
+    fmt.Println("Creating DNS IP Parity Enforcer Chron Job")
     scheduler := gocron.NewScheduler(time.UTC)
     scheduler.Every(10).Minutes().Do(EnforceIpParity)
     scheduler.StartBlocking()
@@ -29,30 +30,50 @@ func EnforceIpParity() {
             fmt.Println("chron job failed in an unexpected way", r)
         }
     }()
-    var resp ApiResp
+    if prevIp == "0.0.0.0"{
+        fmt.Println("Previous DNS Record Unknown, attempting to find IP associated with DNS record")
+        findCurrentDnsIp()
+    }
     body, err := queryForIpAddress()
     if err != nil{
         return
     }
+    var resp ApiResp
     json.Unmarshal(body, &resp)
     if prevIp == resp.Ip {
-        fmt.Println("Ip address is the same. No change to DNS required")
+        fmt.Println("Ip address is the same. No change to DNS required. IP is ", resp.Ip)
     } else{
         changeWebsiteIpAddress(resp)
     }
 
 }
 
+func findCurrentDnsIp(){
+    ips, err := net.LookupIP("mochijump.com")
+    if err != nil {
+        fmt.Println("Error %s", err)
+        return
+    }
+    for _, ip := range ips {
+        // currently restricted to ipv4
+        if ipv4 := ip.To4(); ipv4 != nil {
+            fmt.Println("Dns Ip Address is Currently Set to: ", ipv4)
+            prevIp = fmt.Sprintf("%s", ipv4)
+        }
+    }
+}
+
+
 func queryForIpAddress() ([]byte, error) {
     resp, err := http.Get("https://api.myip.com")
     if err != nil {
-        fmt.Printf("Error %s", err)
+        fmt.Println("Error %s", err)
         return nil, err
     }
     defer resp.Body.Close()
     body, err := ioutil.ReadAll(resp.Body)
     if err != nil {
-        fmt.Printf("Error %s", err)
+        fmt.Println("Error %s", err)
         return nil, err
     }
     return body, nil
@@ -60,16 +81,17 @@ func queryForIpAddress() ([]byte, error) {
 
 func changeWebsiteIpAddress(resp ApiResp){
     fmt.Println("Changing ip address in DNS record to", resp.Ip)
-    url := "https://username:password@domains.google.com/nic/update?hostname=subdomain.domain.com&myip="+resp.Ip
+    host := "mochijump.com"
+    url := "https://user:pass@domains.google.com/nic/update?hostname="+host+"&myip="+resp.Ip
     res, err := http.Get(url)
     if err != nil {
-        fmt.Printf("Error %s", err)
+        fmt.Println("Error %s", err)
         return
     }
     defer res.Body.Close()
     body, err := ioutil.ReadAll(res.Body)
     if err != nil {
-        fmt.Printf("Error %s", err)
+        fmt.Println("Error %s", err)
         return
     }
     //fmt.Println("HTTP status:", res.StatusCode)
